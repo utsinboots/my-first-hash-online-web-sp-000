12196e67b5b5db018900a5decc08c326f6d2305f
Utshant Gurung <utshantgurung@gmail.com>
2021-06-07 16:04:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,6 @@ def shipping_manifest
               "porcelain vases" => 2, 
               "oil paintings" => 3
              }
-
 end
 
 def retrieval
@@ -24,7 +23,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
